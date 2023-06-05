1c4fc34b224da9666458b146b837f5ca7e2d9958
devstudio <8589835+henan-animal-husbandry_0@user.noreply.gitee.com>
2023-06-05 15:08:08 +0000

    init project

--- src/main/scala/edu/hnuahe/lcx/DataLoader.scala
+++ src/main/scala/edu/hnuahe/lcx/DataLoader.scala
@@ -1,7 +1,8 @@
 package edu.hnuahe.lcx
 
+import com.mongodb.casbah.Imports._
 import com.mongodb.casbah.commons.MongoDBObject
-import com.mongodb.casbah.{MongoClient, MongoClientURI}
+//import com.mongodb.casbah.{MongoClient, MongoClientURI}
 //import com.mongodb.client.model.Collation
 //import com.mongodb.client.MongoClient
 import org.apache.spark.SparkConf
